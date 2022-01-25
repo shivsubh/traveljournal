091abf6297930ea690d0b91ce957e6a80cf232bb
Shubham <shubhamshivaay67@gmail.com>
2022-01-26 00:49:01 +0530

    react project 2

---
+++ src/components/Cards.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Card(props) {
+    return (
+        <div className="card">
+            <div>
+                 <img src={`../images/${props.item.imageUrl}`} className="card--img" />
+            </div>
+            <div>
+                <img src="../images/location-logo.png" className="card--locationimg" />
+                <span className="card--location">{props.item.location}</span>
+                <a href={props.item.googleMapsUrl} className="card--gmap">View on Google Maps</a>
+                <h1 className="card--title>">{props.item.title}</h1>
+                <span className="card--date">{props.item.startDate} - {props.item.endDate}</span>
+                <p className="card--des">{props.item.description}</p>
+            </div>
+
+        </div>
+    )
+}
